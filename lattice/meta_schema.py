2b894f4aa6eb6b459f73d69c391adc74cbb6b3c8
tanaya-bigladder <57679220+tanaya-bigladder@users.noreply.github.com>
2022-09-27 12:52:38 -0600

    Create build dir if it doesn't exist.

--- lattice/meta_schema.py
+++ lattice/meta_schema.py
@@ -276,6 +276,8 @@ def get_types(schema):
 
 def generate_meta_schemas(output_schema_list, root_schema_list):
     for output_schema, root_schema in zip(output_schema_list, root_schema_list):
+        if not os.path.isdir(os.path.dirname(output_schema)):
+            os.mkdir(os.path.dirname(output_schema))
         generate_meta_schema(output_schema, root_schema)
 
 
