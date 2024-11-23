7f40f6fa0bbc3fd8858bfb6696d57b9293f6a905
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2024-11-23 15:28:10 -0700

    Load module by path.

--- lattice/cpp/header_entry_plugin_loader.py
+++ lattice/cpp/header_entry_plugin_loader.py
@@ -1,4 +1,4 @@
-import importlib
+from importlib import util
 from pathlib import Path
 
 """From https://github.com/ArjanCodes/2021-plugin-architecture/blob/main/after/game/loader.py"""
@@ -11,13 +11,19 @@ class ModuleInterface:
         """Register the necessary items in the game character factory."""
 
 
-def import_module(name: str) -> ModuleInterface:
+def import_module(path: Path) -> ModuleInterface:
     """Imports a module given a name."""
-    return importlib.import_module(name)  # type: ignore
-
+    name = path.stem
+    spec = util.spec_from_file_location(name, path)
+    try:
+        module = util.module_from_spec(spec)
+        spec.loader.exec_module(module)
+        return module
+    except:
+        raise ModuleNotFoundError
 
 def load_plugins(plugins: Path) -> None:
     """Loads the plugins defined in the plugins list."""
     for plugin_file in plugins.iterdir():
-        plugin = import_module(plugin_file.stem) #TODO: not robust?
+        plugin = import_module(plugin_file) #TODO: not robust?
         plugin.register()
