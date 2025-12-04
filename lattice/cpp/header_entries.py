24fa6840dc6bbc4c714eaf69f95844c40a27a298
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2025-12-04 12:33:22 -0700

    Format f-string correctly for python <3.12.

--- lattice/cpp/header_entries.py
+++ lattice/cpp/header_entries.py
@@ -306,9 +306,11 @@ class DataElement(HeaderEntry):
                 for base_type in self._referenced_datatypes:
                     assert custom_type.superclass_name is not None
                     if base_type.name == custom_type.superclass_name:
-                        self.type = f"std::unique_ptr<{
-                            DataElement.make_scoped((base_type.namespace, custom_type.superclass_name))
-                        }>"
+                        self.type = (  # There are some vagaries in f-string multiline formatting in py3.12+
+                            # Use parens plus begin & end quotes for maximum 7compatibility.
+                            "std::unique_ptr<"
+                            f"{DataElement.make_scoped((base_type.namespace, custom_type.superclass_name))}>"
+                        )
                         break
 
     __hash__ = HeaderEntry.__hash__
